cdde3a5f1b5450a508de3de8df84d43da9daf36f
wuxunjiee <595850318@qq.com>
2022-06-02 18:33:18 +0800

    feat(template): 对模板进行初步处理

---
+++ src/compiler/index.js
@@ -0,0 +1,1 @@
+export function compileToFunction(template) {}
--- src/init.js
+++ src/init.js
@@ -1,4 +1,5 @@
 import { initState } from "./state";
+import { compileToFunction } from "./compiler";
 
 export function initMixin(Vue) {
   /**
@@ -8,5 +9,28 @@ export function initMixin(Vue) {
     const vm = this;
     vm.$options = options;
     initState(vm);
+    if (options.el) {
+      vm.$mount(options.el);
+    }
+  };
+
+  Vue.prototype.$mount = function (el) {
+    const vm = this;
+    const el = document.querySelector(el);
+    const opts = vm.$options;
+    if (!opts.render) {
+      let template;
+      if (el) {
+        if (!opts.template) {
+          template = el.outerHTML;
+        } else {
+          template = opts.template;
+        }
+        if (template) {
+          const render = compileToFunction(template);
+          opts.render = render;
+        }
+      }
+    }
   };
 }
